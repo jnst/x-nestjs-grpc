aeacb62f955e7e72066f2f1c656f403d06283275
jnst <jnst@outlook.com>
2020-01-21 20:08:04 +0900

    Fix test

--- src/rpc/rpc.controller.spec.ts
+++ src/rpc/rpc.controller.spec.ts
@@ -1,16 +1,19 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {RpcController} from './rpc.controller';
+import {ChampionService} from '../champion/champion.service';
+import {BattleFieldService} from '../battle-field/battle-field.service';
 
 describe('Rpc Controller', () => {
-  let module: TestingModule;
+  let controller: RpcController;
 
   beforeAll(async () => {
-    module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [RpcController],
+      providers: [ChampionService, BattleFieldService],
     }).compile();
+    controller = module.get<RpcController>(RpcController);
   });
   it('should be defined', () => {
-    const controller: RpcController = module.get<RpcController>(RpcController);
     expect(controller).toBeDefined();
   });
 });
